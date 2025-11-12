1676a9f3873a4e87d37f2601ea4b53e71424b68d
cbdowney09-alt <cbdowney09@gmail.com>
2025-11-12 18:17:55 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,17 @@
 const CACHE_NAME = 'workproof-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
+      .catch((err) => console.error('SW install cache failed', err))
   );
 });
 
